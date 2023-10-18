c03bfff1937eea6d32b5cca80cd2429c4803c5ae
Ayush-Sharma1 <sharmaayush2005@gmail.com>
2023-10-18 23:56:44 +1300

    18/10/23

--- player.gd
+++ player.gd
@@ -1,14 +1,21 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const SPRINT = 10
-var speed =  5.0
+const SPEED = 20
+const JUMP_VELOCITY = 8
+const SPRINT = 25
+var speed = 20
+var health = HEALTH
+const HEALTH = 100
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var neck = $Neck
 @onready var camera = $Neck/Camera3D
+@onready var player = $"."
+
+func _ready():
+	$ProgressBar.value = HEALTH
+
 
 func _unhandled_input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -27,19 +34,23 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		
 		
 	#handle sprint
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT
+		
 	else:
-		speed = SPEED 
-
+		speed = SPEED
+		
+		
+	$ProgressBar.value = health 
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("right", "left", "back", "forward")
+	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	var direction = (neck.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * speed
@@ -49,3 +60,8 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, speed)
 
 	move_and_slide()
+	
+
+func _on_enemy_damage():
+	if health == 0:
+		print("e")
